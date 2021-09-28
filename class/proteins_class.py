06dcb06401c9197e7d8a10d736c5fec6400ef231
Danigore25 <73318665+Danigore25@users.noreply.github.com>
2021-09-28 12:43:16 -0500

    "Se creó el archivo de ejercicios de clase proteins_class.py"

--- class/proteins_class.py
+++ class/proteins_class.py
@@ -25,6 +25,21 @@ residue1 = chain[1]
 print("Nombre del primer residuo: ", residue1.get_resname())
 print("ID del primer residuo: ", residue1.get_id()[1])
 
+residue2 = chain[22]
+
+for atom in residue2:
+    print(atom)
+
+atom_ca = residue2['CB']
+
+print(atom_ca)
+
+print(atom_ca.coord)
+
+print(atom_ca.element)
+print(atom_ca.id)
+
+
 # ARCHIVO 1kcw.pdb
 
 # obj_struc = parser.get_structure("prot_1kcw", "../docs/1kcw.pdb")
@@ -40,5 +55,19 @@ print("ID del primer residuo: ", residue1.get_id()[1])
 #                 if residuo.get_resname() == 'CYS':
 #                     cisteinas.append(residuo)
 #
+# EJERCICIO 3
+#
+# for atom2 in cisteinas[0]:
+#     print(atom2.element, atom2.id)
+#
+# EJERCICIO 4
+#
+# pares = []
+# for cisteina_1 in cisteinas:
+#     for cisteina_2 in cisteinas:
+#         if not (cisteina_1 == cisteina_2):
+#             if (cisteina_1['SG'] - cisteina_2['SG']) < 8:
+#                 print(cisteina_1.coord, cisteina_2.coord)
+#                 print(cisteina_1.id, cisteina2.id)
 #
 # print(len(cisteinas))
