74876439506c05c83f07178935eb53ac1a5c4b89
Danigore25 <73318665+Danigore25@users.noreply.github.com>
2021-12-01 10:17:33 -0600

    "Se creó el archivo proyecto_borrador.py"

--- project/proyecto_borrador.py
+++ project/proyecto_borrador.py
@@ -1,23 +1,41 @@
 import argparse
-from Bio import Entrez
+import Bio.Entrez as Entrez
+# import numpy as np
+# import matplotlib.pyplot as plt
+
 Entrez.email = "dgoretti@lcg.unam.mx"
 
 print("Introduce los terminos de busqueda")
+print("Ingrese el pais en el que desea buscar")
+print("Ingrese el año de publicacion que le interesa")
 
 # Paso de parametros por argumentos
 parser = argparse.ArgumentParser(description="Busqueda de terminos en pubmed")
-parser.add_argument("-i", "--input",
+parser.add_argument("-w", "--word",
                     metavar="word of interest",
                     type=str,
                     help="Terminos a buscar",
                     required=True)
 
-args = parser.parse_args()
-search = args.input
+# Search by country and year of publication.
+parser.add_argument("-c", "--country",
+                    metavar="country of publication",
+                    type=str,
+                    help="Pais donde se desean buscar las publicaciones",
+                    required=True)
 
+parser.add_argument("-y", "--year",
+                    metavar="year of publication",
+                    type=str,
+                    help="Fecha de publicacion del articulo",
+                    required=True)
+
+args = parser.parse_args()
+
+search2 = args.country + "[CNTY] AND " + args.year + "[PDAT]"
 
 # through handle the terms in "search" will be looked for in pubmed database
-handle = Entrez.esearch(db="pubmed", term=search)
+handle = Entrez.esearch(db="pubmed", term=args.word)
 
 # The results that coincide with the terms in "search" are stored in result
 result = Entrez.read(handle)
