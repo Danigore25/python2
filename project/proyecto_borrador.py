d532d6e908b710727cffe3807fecf802f1fd1408
Danigore25 <73318665+Danigore25@users.noreply.github.com>
2021-12-01 11:11:56 -0600

    "Se creó el archivo proyecto_borrador.py"

--- project/proyecto_borrador.py
+++ project/proyecto_borrador.py
@@ -1,9 +1,9 @@
 import argparse
-import Bio.Entrez
+import Bio.Entrez as Entrez
 # import numpy as np
 # import matplotlib.pyplot as plt
 
-Bio.Entrez.email = "dgoretti@lcg.unam.mx"
+Entrez.email = "dgoretti@lcg.unam.mx"
 
 print("Introduce los terminos de busqueda")
 print("Ingrese el pais en el que desea buscar")
@@ -32,13 +32,13 @@ parser.add_argument("-y", "--year",
 
 args = parser.parse_args()
 
-search2 = args.country + "[CNTY] AND " + args.year + "[PDAT]"
+search = args.country + "[CNTY] AND " + args.year + "[PDAT] AND (" + args.word + ")"
 
 # through handle the terms in "search" will be looked for in pubmed database
-handle = Bio.Entrez.esearch(db="pubmed", term=args.word)
+handle = Entrez.esearch(db="pubmed", term=search)
 
 # The results that coincide with the terms in "search" are stored in result
-result = Bio.Entrez.read(handle)
+result = Entrez.read(handle)
 
 # in archiv, the list with the corresponding IDs will be stored
 archiv = result["IdList"]
